4f65a3634246bb2a8d663c33a868e1c0e202cdd1
sabin411 <sherasabin411@gmail.com>
2022-01-03 14:44:05 +0545

    FIX #1 REST API

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { PrismaClient } from '@prisma/client';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService, PrismaClient],
 })
---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,35 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Prisma } from '@prisma/client';
+import { Exclude } from 'class-transformer';
+import {
+  IsNotEmpty,
+  IsOptional,
+  MaxLength,
+  Min,
+  MinLength,
+  minLength,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsNotEmpty()
+  @MinLength(3)
+  @ApiProperty()
+  name: string;
+
+  @IsOptional()
+  @MaxLength(150)
+  @ApiProperty({ required: false })
+  description?: string;
+
+  @Min(1.0)
+  @ApiProperty()
+  price: number;
+
+  @IsNotEmpty()
+  @MinLength(5)
+  @ApiProperty()
+  sku: string;
+
+  @ApiProperty({ required: false, default: false })
+  published?: boolean;
+}
